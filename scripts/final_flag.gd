043ea937a063e6681908df6e63fd08f418cb39d2
Falling Git <golyandringraf@gmail.com>
2023-09-17 23:11:16 +0300

    убрал баг

--- scripts/final_flag.gd
+++ scripts/final_flag.gd
@@ -13,5 +13,4 @@ func _on_sprite_animation_finished() -> void:
 		Root.Win.emit()
 		get_tree().paused = true
 		add_child(win_menu.instantiate())
-		if Root.currect_level < 50:
-			Root.currect_level += 1
+
